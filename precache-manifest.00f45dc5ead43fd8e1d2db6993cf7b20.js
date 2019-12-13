53a788513dad4dbed030b3e9879456970912fd27
chandan193 <chandanahuja93@gmail.com>
2019-12-13 17:59:51 +0530

    Updates

---
+++ precache-manifest.00f45dc5ead43fd8e1d2db6993cf7b20.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9658a9a5073963f4da1d042a727fb22f",
+    "url": "/youtubeapp/index.html"
+  },
+  {
+    "revision": "89bf95a6452243c83902",
+    "url": "/youtubeapp/static/css/main.c53ef842.chunk.css"
+  },
+  {
+    "revision": "583c2d6cc9316d153b53",
+    "url": "/youtubeapp/static/js/2.9b4dedf2.chunk.js"
+  },
+  {
+    "revision": "89bf95a6452243c83902",
+    "url": "/youtubeapp/static/js/main.2f681661.chunk.js"
+  },
+  {
+    "revision": "17509d8f6ce111f4c9c0",
+    "url": "/youtubeapp/static/js/runtime-main.84dc3caa.js"
+  }
+]);
